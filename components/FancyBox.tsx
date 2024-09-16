dcaf06a1491d3b488b59d71fa05ba4ad42e43447
Bill Mårtensson <bill@magictechnology.se>
2024-09-16 12:52:08 +0200

    v2 start

---
+++ components/FancyBox.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function FancyBox() {
+    return (
+        <View style={{ backgroundColor: "grey", width: 50, height: 50}}>
+            <Text>Fancy</Text>
+        </View>
+    );
+}
+
+
