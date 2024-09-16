a593310c12a53cbb9184b765e939823483a2e7ed
billmartensson <bill@magictechnology.se>
2024-09-16 13:14:22 +0200

    Update index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -9,7 +9,7 @@ import FancyBox from '@/components/FancyBox';
 
 export default function HomeScreen() {
 
-  const [fruit, setFruit] = useState("Päron");
+  const [fruit, setFruit] = useState("Avokado");
 
   const [counter, setCounter] = useState(0);
 
